00c8b0ac4fc1d0a187b6a1b11e144de37102c48d
gfkl <gfkl@localhost.localdomain>
2021-01-23 02:49:31 +0400

    added homework

---
+++ multipleOfTen/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace multipleOfTen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+
+            int countMultipleOfTen = 0;
+            
+            //masivis shevseba 
+            for(int i = 0; i < arr.Length; i++){
+                Console.Write("shemoiyvanet ricxvi: ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+                if (arr[i] % 10 == 0 && arr[i] % 3 == 0){
+                    countMultipleOfTen++;
+                }
+            }
+
+            Console.WriteLine($"masivshi atisa da samis jeradi ricxvebis raodenobaa:  {countMultipleOfTen}");
+
+        }
+    }
+}
---
+++ searchinlist/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace searchinlist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> libr = new List<int>(){23,123,24,53,1,22,5,46,4,21,64,12,3,4,5,1,2,8,67,45,363,234,123,546,867,435};
+            string result = "";
+            while(true)
+            {
+                Console.Write("sheiyvanet sadziebo ricxvi: ");
+                int input = Convert.ToInt32(Console.ReadLine());
+                for(int i = 0; i < libr.Count; i++){
+                    if (libr[i] == input){
+                        result = $"siashi aris ricxvi indexze -- {i}";
+                        break;
+                    } else {
+                        result = "-1";
+                    }
+                }
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
